5bc6e879ae2f7ad2321e2fd66be621fa9d5e8906
Florent Kermarrec <florent@enjoy-digital.fr>
2017-06-23 09:14:19 +0200

    update litescope

--- example_designs/test/test_logic_analyzer.py
+++ example_designs/test/test_logic_analyzer.py
@@ -5,20 +5,16 @@ from litex.soc.tools.remote import RemoteClient
 wb = RemoteClient()
 wb.open()
 
-logic_analyzer = LiteScopeLogicAnalyzerDriver(wb.regs, "logic_analyzer", debug=True)
+analyzer = LiteScopeLogicAnalyzerDriver(wb.regs, "logic_analyzer", debug=True)
 
 # # #
 conditions = {}
-logic_analyzer.configure_term(port=0, cond=conditions)
-logic_analyzer.configure_sum("term")
+analyzer.configure_trigger(cond={})
 # run logic analyzer
-logic_analyzer.run(offset=2048, length=4000)
-
-while not logic_analyzer.done():
-    pass
-
-logic_analyzer.upload()
-logic_analyzer.save("dump.vcd")
+analyzer.run(offset=2048, length=4000)
+analyzer.wait_done()
+analyzer.upload()
+analyzer.save("dump.vcd")
 
 # # #
 
