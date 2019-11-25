10a911088c25815c30b0b0d5fe7e4e211aac616f
Florent Kermarrec <florent@enjoy-digital.fr>
2019-11-25 08:53:40 +0100

    test: rename test_liteeth_gen to test_gen and call gen.py instead of liteeth_gen

--- test/test_gen.py
+++ test/test_gen.py
@@ -6,10 +6,10 @@ import os
 
 root_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..")
 
-class TestLiteDRAMGen(unittest.TestCase):
+class TestGen(unittest.TestCase):
     def test(self):
         for phy in ["mii", "gmii", "rgmii"]:
             for core in ["wishbone", "udp"]:
                 os.system("rm -rf {}/build".format(root_dir))
-                os.system("liteeth_gen --phy={} --core={}".format(phy, core))
+                os.system("python3 {}/liteeth/gen.py --phy={} --core={}".format(root_dir, phy, core))
                 self.assertEqual(os.path.isfile("{}/build/gateware/liteeth_core.v".format(root_dir)), True)
