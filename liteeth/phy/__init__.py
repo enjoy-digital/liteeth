32d4af1148d59939fdf04b4ba319c83427b698c3
Florent Kermarrec <florent@enjoy-digital.fr>
2020-03-01 20:13:23 +0100

    phy/__init__: import all phys.

--- liteeth/phy/__init__.py
+++ liteeth/phy/__init__.py
@@ -25,6 +25,13 @@ def LiteEthPHY(clock_pads, pads, clk_freq=None, **kwargs):
 from liteeth.phy.mii  import LiteEthPHYMII
 from liteeth.phy.rmii import LiteEthPHYRMII
 from liteeth.phy.gmii import LiteEthPHYGMII
+from liteeth.phy.gmii_mii import LiteEthPHYGMIIMII
 
+from liteeth.phy.s6rgmii import LiteEthPHYRGMII as LiteEthS6PHYRGMII
 from liteeth.phy.s7rgmii   import LiteEthPHYRGMII as LiteEthS7PHYRGMII
+from liteeth.phy.usrgmii   import LiteEthPHYRGMII as LiteEthUSPHYRGMII
 from liteeth.phy.ecp5rgmii import LiteEthPHYRGMII as LiteEthECP5PHYRGMII
+
+from liteeth.phy.a7_1000basex import A7_1000BASEX
+from liteeth.phy.k7_1000basex import K7_1000BASEX
+from liteeth.phy.ku_1000basex import KU_1000BASEX
