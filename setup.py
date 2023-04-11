d66457d2b0c2dfdc461658978b909e92a0260a66
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-04-11 14:05:08 -0400

    add manifest, uplift setup.py to pass twine checks

--- setup.py
+++ setup.py
@@ -4,9 +4,16 @@ from setuptools import setup
 from setuptools import find_packages
 
 
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+
 setup(
     name="liteeth",
+    version="2022.12",
     description="Small footprint and configurable Ethernet core",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Florent Kermarrec",
     author_email="florent@enjoy-digital.fr",
     url="http://enjoy-digital.fr",
@@ -14,8 +21,24 @@ setup(
     test_suite="test",
     license="BSD",
     python_requires="~=3.6",
+    install_requires=["pyyaml", "litex", "liteiclink"],
+    extras_require={
+        "develop": [
+          "setuptools"
+        ]
+    },
     packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
     include_package_data=True,
+    keywords="HDL ASIC FPGA hardware design",
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+        "Environment :: Console",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
     entry_points={
         "console_scripts": [
             "liteeth_gen=liteeth.gen:main",
