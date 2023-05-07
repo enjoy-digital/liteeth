d607d9f34bfbae3f6e162078f51ea78319ea79ab
Florent Kermarrec <florent@enjoy-digital.fr>
2023-05-07 20:48:15 +0200

    setup.py: Prepare for 2023.04.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name="liteeth",
-    version="2022.12",
+    version="2023.04",
     description="Small footprint and configurable Ethernet core",
     long_description=long_description,
     long_description_content_type="text/markdown",
