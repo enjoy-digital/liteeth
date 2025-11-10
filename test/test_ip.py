0e76c248387e5f8f138e6ad7b29af74631948101
Florent Kermarrec <florent@enjoy-digital.fr>
2025-11-10 09:25:28 +0100

    test/test_ip: Update/Cleanup.

--- test/test_ip.py
+++ test/test_ip.py
@@ -18,20 +18,24 @@ from test.model import phy, mac, arp, ip
 
 from litex.gen.sim import *
 
-ip_address = 0x12345678
+# Constants ----------------------------------------------------------------------------------------
+
+ip_address  = 0x12345678
 mac_address = 0x12345678abcd
 
+# DUT ----------------------------------------------------------------------------------------------
 
-class DUT(Module):
+class DUT(LiteXModule):
     def __init__(self):
-        self.submodules.phy_model = phy.PHY(8, debug=False)
-        self.submodules.mac_model = mac.MAC(self.phy_model, debug=False, loopback=False)
-        self.submodules.arp_model = arp.ARP(self.mac_model, mac_address, ip_address, debug=False)
-        self.submodules.ip_model = ip.IP(self.mac_model, mac_address, ip_address, debug=False, loopback=True)
+        self.phy_model = phy.PHY(8, debug=False)
+        self.mac_model = mac.MAC(self.phy_model, debug=False, loopback=False)
+        self.arp_model = arp.ARP(self.mac_model, mac_address, ip_address, debug=False)
+        self.ip_model  = ip.IP(self.mac_model, mac_address, ip_address, debug=False, loopback=True)
 
-        self.submodules.ip = LiteEthIPCore(self.phy_model, mac_address, ip_address, 100000)
+        self.ip = LiteEthIPCore(self.phy_model, mac_address, ip_address, 100000)
         self.ip_port = self.ip.ip.crossbar.get_port(udp_protocol)
 
+# Generator ----------------------------------------------------------------------------------------
 
 def main_generator(dut):
     yield dut.ip_port.sink.valid.eq(1)
@@ -45,16 +49,19 @@ def main_generator(dut):
     print("packet from IP 0x{:08x}".format((yield dut.ip_port.sink.ip_address)))
 
 
+# Test IP ------------------------------------------------------------------------------------------
+
 class TestIP(unittest.TestCase):
     def test(self):
         dut = DUT()
         generators = {
-            "sys" :   [main_generator(dut)],
-            "eth_tx": [dut.phy_model.phy_sink.generator(),
-                       dut.phy_model.generator()],
-            "eth_rx":  dut.phy_model.phy_source.generator()
+            "sys"    : [main_generator(dut)],
+            "eth_tx" : [dut.phy_model.phy_sink.generator(), dut.phy_model.generator()],
+            "eth_rx" : [dut.phy_model.phy_source.generator()],
         }
-        clocks = {"sys":    10,
-                  "eth_rx": 10,
-                  "eth_tx": 10}
+        clocks = {
+            "sys"    : 10,
+            "eth_rx" : 10,
+            "eth_tx" : 10,
+        }
         run_simulation(dut, generators, clocks, vcd_name="sim.vcd")
